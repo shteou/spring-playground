f391162c591721a0ef2717cfc422685800cec679
Stewart Platt <shteou@gmail.com>
2019-11-08 16:14:08 +0000

    Use reactive components for hello controller

--- src/main/java/com/stewartplatt/user/controllers/HelloController.java
+++ src/main/java/com/stewartplatt/user/controllers/HelloController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import reactor.core.publisher.Mono;
+
 @RestController
 public class HelloController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String hello() {
-        return "hello";
+    public Mono<String> hello() {
+        return Mono.just("hello");
     }
 }
